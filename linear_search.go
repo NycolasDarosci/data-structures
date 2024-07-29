b3bbed4dda20f5b3c24b7162dc1b2930008fb6a3
Nycolas Darosci <Robert8765@protonmail.com>
2024-07-29 20:37:53 -0300

    linear seach on an ordered array

---
+++ linear_search.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+/*
+	__Linear Search
+	check each cell one at a time — from left to right -
+	until find the value
+
+	__Linear Search on an ordered array
+	check each cell one at a time — from left to right -
+	until find the value, but the value is compared with the
+	values within the array.
+		Is value input greater than the value? search for next
+		Is value input greater than the value? no
+		stop the search
+*/
+
+// linear search on an ordered array
+func DoLinearSearch() {
+	arr := []int{1, 3, 7, 15, 22, 78}
+	searchValue := 7 // 2
+	value := linearSearch(arr, searchValue)
+	fmt.Println(*value)
+}
+
+func linearSearch(arr []int, searchValue int) *int {
+	for index, value := range arr {
+		if value == searchValue {
+			return &index
+		} else if value > searchValue {
+			break
+		}
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -1,14 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	number := 2
-	for number <= 100 {
-		if number%2 == 0 {
-			fmt.Println(number)
-		}
-		number += 1
-	}
-
+	DoLinearSearch()
 }
